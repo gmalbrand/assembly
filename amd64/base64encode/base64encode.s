fd370d747968ee6aaf82cc8503ecde4041a575c7
Guillaume Malbrand <gmalbrand@free.fr>
2023-09-20 09:12:14 +0000

    Clean code and add comments

--- amd64/base64encode/base64encode.s
+++ amd64/base64encode/base64encode.s
@@ -4,6 +4,9 @@
 ** Encode in base64 content read on STDIN and print it to STDOUT
 **
 */
+.section .rodata
+b64chars:
+    .ascii "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"    
 .text
 .global _start
 _start:
@@ -17,31 +20,10 @@ _start:
     **         8 bytes to store override length -16(%rbp)
     **         8 bytes to store current position in message -24(%rbp)
     **         8 bytes to store result length -32(%rbp)
-    **        64 bytes to store the dictionnary -96(%rbp) : 
-    **            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-    **      1024 bytes to read from stdin  -1096(%rbp) or 1364(%rsp)
-    **      1364 bytes to write base64 message -2460(%rbp) or (%rsp)
+    **      1024 bytes to read from stdin 1364(%rsp)
+    **      1364 bytes to write base64 message (%rsp)
     */
-    subq    $32,                %rsp
-    
-    movabs  $0x2f2b393837363534,%rax
-    push    %rax
-    movabs  $0x333231307a797877,%rax
-    push    %rax
-    movabs  $0x767574737271706f,%rax
-    push    %rax
-    movabs  $0x6e6d6c6b6a696867,%rax
-    push    %rax
-    movabs  $0x6665646362615a59,%rax
-    push    %rax
-    movabs  $0x5857565554535251,%rax
-    push    %rax
-    movabs  $0x504f4e4d4c4b4a49,%rax
-    push    %rax
-    movabs  $0x4847464544434241,%rax
-    push    %rax
-
-    subq    $2388,          %rsp        
+    subq    $2420,              %rsp        
     /*
     ** Read input on STDIN
     */
@@ -54,71 +36,58 @@ read_input:
     
     mov     %rax,               -8(%rbp)    /* Copy read length to message size */
 
-    cmp    $0,                 %rax
-    je      end_encode
+    cmp     $0,                 %rax         /* If  nothing on stdin jump to end */
+    je      exit_0
 
-    /*
-    ** Convert in b64 
-    */            
-    xor     %rdx,               %rdx
+    xor     %rdx,               %rdx        /* Compute result length and overrid */
     mov     $3,                 %rbx        /* Set divisor, dividend is already in rax */
     div     %rbx                            /* Divide message length by 3 */
-    mov     %rdx,               %rdi
-    /* Compute result length */
-    mov     $4,                 %rbx
+    mov     %rdx,               %rdi        /* Save remainded  */
+    mov     $4,                 %rbx        /* Multiply quotient by 4 to get result size */
     mul     %rbx
-    mov     %rax,               -32(%rbp)
-    
-    cmp     $0,                 %rdi
-    je      encode                          /* if length % 3 = 0 no need to 
-                                                pad with = at end of encode message */
-    addq    $4,                 -32(%rbp)
-    movq    $3,                 -16(%rbp)   /* Compute required padding */
-    subq    %rdi,               -16(%rbp)
+    mov     %rax,               -32(%rbp)   /* Save result length */
+    cmp     $0,                 %rdi        /* If padding required */
+    je      encode                         
+    addq    $4,                 -32(%rbp)   /* Add 4 to message length */
+    movq    $3,                 -16(%rbp)   /* Compute override */
+    subq    %rdi,               -16(%rbp)   
 
-    /* 
-    ** Remmember to fill padding with 0 
-    */
 encode:                                     /* Initialize loop */        
-    movq    $0,                 -24(%rbp)
-    mov     $0,                 %rdx        /* Output position */
-
+    movq    $0,                 -24(%rbp)   /* Current message position */
+    mov     $0,                 %rdx        /* Current Output position */
+    leaq    b64chars,           %rbx        /* Address of b64chars */
 encoding_loop:
     xor     %rax,               %rax        /* Set rax to 0 */
-    xor     %rdi,               %rdi
+    xor     %rdi,               %rdi        /* Set rdi to 0 */
     movq    -24(%rbp),          %rdi        /* Get read position */
-    mov     $3,                 %rcx
+    mov     $3,                 %rcx        /* Move bytes to big endian */
 big_endian:
-    movb    1364(%rsp, %rdi),   %al        /* Get next 3 bytes big endian */
+    movb    1364(%rsp, %rdi),   %al
     shl     $8,                 %rax
     inc     %rdi
     loop    big_endian
     shr     $8,                 %rax
-
-    mov     $4,                 %rcx        /* Set loop countdown to 4 */
+            
+    mov     $4,                 %rcx        /* Encode next 3 bytes */
 get_c:
-    movl    %eax,               %esi
-    shr     $18,                %esi
-    and     $0x3F,              %esi
-    movb    -96(%rbp,%rsi),     %sil
-    movb    %sil,               (%rsp,%rdx)
-    shl     $6,                 %eax
-    inc     %rdx
+    movl    %eax,               %esi        /* Copy bytes */
+    shr     $18,                %esi        /* Keep only 6 higher bits */
+    and     $0x3F,              %esi        /* Only keep 6 bits */
+    movb    (%rbx,%rsi),        %sil        /* Get corresponding char */
+    movb    %sil,               (%rsp,%rdx) /* Mov to char to result */
+    shl     $6,                 %eax        /* Move to next 6 bits */
+    inc     %rdx                            /* Increment result position */
     loop    get_c
 
-    /* loop condition */
-    addq    $3,                 -24(%rbp)   /* increment position by 3 */   
+    addq    $3,                 -24(%rbp)   /* Increment message position by 3 */   
     movq    -8(%rbp),           %rax        
     cmpq    %rax,               -24(%rbp)   /* Compare position to message length */
-    jl      encoding_loop
+    jl      encoding_loop                   /* Jump if there are bytes left */
     
-    /*
-    ** Padding with = if required
-    */
-    movq    -16(%rbp),          %rcx
-    cmp     $0,                 %rcx
+    movq    -16(%rbp),          %rcx        /* Check if padding is required */
+    cmp     $0,                 %rcx        
     je      end_encode
-    movq    -32(%rbp),          %rax
+    movq    -32(%rbp),          %rax        /* Add = * override at end of result */
     subq    %rcx,               %rax
 padding:
     movb    $0x3d,              (%rsp,%rax)
@@ -126,9 +95,7 @@ padding:
     loop    padding
 
 end_encode:
-
-    /* Write on STDOUT */
-    mov     $1,                 %rax
+    mov     $1,                 %rax        /* Write result on stdout */
     mov     $1,                 %rdi
     leaq    (%rsp),             %rsi        /* Move down the stack with buf_len!! */
     mov     -32(%rbp),          %rdx
@@ -136,14 +103,6 @@ end_encode:
     
     cmpq    $1023,              -8(%rbp)    /* Check if there is still data on stdin */ 
     je      read_input
-
-
-    push    $0xa
-    mov     $1,                 %rax
-    mov     $1,                 %rdi
-    leaq    (%rsp),             %rsi
-    mov     $1,                 %rdx
-    syscall
 
 exit_0:
     /* Exit 0 */
